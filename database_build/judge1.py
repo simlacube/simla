a5f752cdc8c34a958ebbe983e8ad684880afb81f
GrantPDonnelly <grantdonnelly2@gmail.com>
2026-03-12 14:59:39 -0400

    update imports

--- database_build/judge1.py
+++ database_build/judge1.py
@@ -23,7 +23,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 from simla_variables import SimlaVar
-from simladb import query, DB_bcd, DB_bcdwise, DB_shardpos, setup_judge1, scorners
+from simladb import query, DB_bcd, DB_bcdwise, DB_shardpos, setup_judge1, simladbX
 from simla_utils import make_wise_bg, DN_to_MJypsr, photometry, fmt_scorners
 
 wisepath = SimlaVar().wisepath
